840a52bea8bfd8f1003639f294479e90e5c79afa
Alexei <alexei.artsimovich@mobexs.com>
2018-01-05 20:04:05 +0300

    created fragment for activities of project

--- app/src/main/java/com/example/paralect/easytime/main/projects/project/ActivityFragment.java
+++ app/src/main/java/com/example/paralect/easytime/main/projects/project/ActivityFragment.java
@@ -1,20 +1,53 @@
 package com.example.paralect.easytime.main.projects.project;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBar;
+import android.support.v7.widget.RecyclerView;
 import android.view.Gravity;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.DatePicker;
 import android.widget.TextView;
 
 import com.example.paralect.easytime.R;
+import com.example.paralect.easytime.main.BaseFragment;
+import com.example.paralect.easytime.views.EmptyRecyclerView;
+
+import org.w3c.dom.Text;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * Created by alexei on 27.12.2017.
  */
 
-public class ActivityFragment extends Fragment {
+public class ActivityFragment extends BaseFragment implements DatePickerDialog.OnDateSetListener {
+
+    @BindView(R.id.date)
+    TextView date;
+
+    @BindView(R.id.activityList)
+    EmptyRecyclerView emptyRecyclerView;
+
+    @BindView(R.id.emptyListPlaceholder)
+    View emptyListPlaceholder;
+
+    @OnClick(R.id.date)
+    void onChooseDate(View view) {
+        DatePickerDialog datePickerDialog = new DatePickerDialog(getContext(), this, 2017, 11, 30);
+        datePickerDialog.show();
+    }
 
     public static ActivityFragment newInstance() {
         return new ActivityFragment();
@@ -23,5 +56,38 @@ public class ActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_project_activity, parent, false);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+
+    }
+
+    @Override
+    public void onCreateActionBar(ActionBar actionBar) {
+
+    }
+
+    @Override
+    public boolean needsOptionsMenu() {
+        return false;
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        ButterKnife.bind(this, view);
+        emptyRecyclerView.setEmptyView(emptyListPlaceholder);
+    }
+
+    @Override
+    public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
+        // open activity
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.MONTH, i1);
+        calendar.set(Calendar.DAY_OF_MONTH, i2);
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("MMMM D", Locale.US);
+        String dateString = simpleDateFormat.format(calendar.getTime());
+        date.setText(dateString);
     }
 }
