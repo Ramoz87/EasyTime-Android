1a171676d364b9827d58907c18197b0df05e5142
Alexei <alexei.artsimovich@mobexs.com>
2018-01-12 16:27:25 +0300

    materials screen and keypad view update

--- app/src/main/java/com/example/paralect/easytime/main/materials/chooser/MaterialEditorFragment.java
+++ app/src/main/java/com/example/paralect/easytime/main/materials/chooser/MaterialEditorFragment.java
@@ -2,22 +2,28 @@ package com.example.paralect.easytime.main.materials.chooser;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.example.paralect.easytime.R;
 import com.example.paralect.easytime.main.BaseFragment;
+import com.example.paralect.easytime.views.KeypadView;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 /**
  * Created by alexei on 12.01.2018.
  */
 
-public class MaterialEditorFragment extends BaseFragment {
+public class MaterialEditorFragment extends BaseFragment implements KeypadView.OnKeypadItemClickListener {
+
+    @BindView(R.id.keypad) KeypadView keypadView;
 
     public static MaterialEditorFragment newInstance() {
         return new MaterialEditorFragment();
@@ -32,6 +38,7 @@ public class MaterialEditorFragment extends BaseFragment {
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         ButterKnife.bind(this, view);
+        keypadView.setOnKeypadItemClickListener(this);
     }
 
     @Override
@@ -47,5 +54,20 @@ public class MaterialEditorFragment extends BaseFragment {
     @Override
     public boolean needsOptionsMenu() {
         return false;
+    }
+
+    @Override
+    public void onNextClick() {
+        Toast.makeText(getContext(), "Clicked on next", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onDeleteClick() {
+        Toast.makeText(getContext(), "Clicked on delete", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNumberClick(int number) {
+        Toast.makeText(getContext(), "Clicked on " + number, Toast.LENGTH_SHORT).show();
     }
 }
--- app/src/main/java/com/example/paralect/easytime/views/KeypadView.java
+++ app/src/main/java/com/example/paralect/easytime/views/KeypadView.java
@@ -5,10 +5,10 @@ import android.os.Build;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.annotation.RequiresApi;
+import android.support.v7.widget.GridLayout;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
-import android.widget.FrameLayout;
 import android.widget.TextView;
 
 import com.example.paralect.easytime.R;
@@ -22,14 +22,29 @@ import butterknife.ButterKnife;
  * Created by alexei on 12.01.2018.
  */
 
-public class KeypadView extends ExpandableLayout {
+public class KeypadView extends ExpandableLayout implements View.OnClickListener {
     private static final String TAG = KeypadView.class.getSimpleName();
+
+    @BindView(R.id.keypadContent)
+    GridLayout keypadContent;
 
     @BindView(R.id.keypadNext)
     View next;
 
     @BindView(R.id.keypadDelete)
     View delete;
+
+    public void setOnKeypadItemClickListener(OnKeypadItemClickListener onKeypadItemClickListener) {
+        this.onKeypadItemClickListener = onKeypadItemClickListener;
+    }
+
+    public interface OnKeypadItemClickListener {
+        void onNextClick();
+        void onDeleteClick();
+        void onNumberClick(int number);
+    }
+
+    private OnKeypadItemClickListener onKeypadItemClickListener;
 
     public KeypadView(@NonNull Context context) {
         this(context, null);
@@ -56,6 +71,50 @@ public class KeypadView extends ExpandableLayout {
         if (!isInEditMode()) {
             ButterKnife.bind(this);
         }
+        initNumberItems();
         setExpanded(true, false);
+    }
+
+    private void initNumberItems() {
+        int count = keypadContent.getChildCount();
+        for (int i = 0; i < count; i++) {
+            View child = keypadContent.getChildAt(i);
+            child.setOnClickListener(this);
+            int id = child.getId();
+            if (id == R.id.id0) setupNumberItem(child, 0);
+            if (id == R.id.id1) setupNumberItem(child, 1);
+            if (id == R.id.id2) setupNumberItem(child, 2);
+            if (id == R.id.id3) setupNumberItem(child, 3);
+            if (id == R.id.id4) setupNumberItem(child, 4);
+            if (id == R.id.id5) setupNumberItem(child, 5);
+            if (id == R.id.id6) setupNumberItem(child, 6);
+            if (id == R.id.id7) setupNumberItem(child, 7);
+            if (id == R.id.id8) setupNumberItem(child, 8);
+            if (id == R.id.id9) setupNumberItem(child, 9);
+
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        Log.d(TAG, "on click");
+        if (onKeypadItemClickListener == null) return;
+
+        Object tag = view.getTag();
+        if (tag != null) { // we assume its a number item
+            Log.d(TAG, "clicked view has some tag");
+            TextView numberItem = (TextView) view;
+            Integer number = (Integer) tag;
+            onKeypadItemClickListener.onNumberClick(number);
+        } else if (view.getId() == R.id.keypadNext) {
+            onKeypadItemClickListener.onNextClick();
+        } else if (view.getId() == R.id.keypadDelete) {
+            onKeypadItemClickListener.onDeleteClick();
+        }
+    }
+
+    private void setupNumberItem(View view, int number) {
+        ((TextView) view).setText(String.valueOf(number));
+        view.setTag(number);
     }
 }
