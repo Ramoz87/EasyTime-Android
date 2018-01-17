9d975132c1dd9b9bb4da6e771ad4c1e77f3f8127
Alexei <alexei.artsimovich@mobexs.com>
2018-01-17 13:57:43 +0300

    added binding material items

--- app/src/main/java/com/example/paralect/easytime/main/projects/project/jobexpenses/materials/MaterialExpensesAdapter.java
+++ app/src/main/java/com/example/paralect/easytime/main/projects/project/jobexpenses/materials/MaterialExpensesAdapter.java
@@ -1,15 +1,18 @@
 package com.example.paralect.easytime.main.projects.project.jobexpenses.materials;
 
+import android.content.res.Resources;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.paralect.easytime.R;
 import com.example.paralect.easytime.model.Material;
 
 import java.util.List;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 /**
@@ -48,13 +51,20 @@ public class MaterialExpensesAdapter extends RecyclerView.Adapter<MaterialExpens
 
     static class ViewHolder extends RecyclerView.ViewHolder {
 
+        @BindView(R.id.materialName) TextView materialName;
+        @BindView(R.id.materialNumber) TextView materialNumber;
+
+        Resources res;
+
         public ViewHolder(View itemView) {
             super(itemView);
             ButterKnife.bind(this, itemView);
+            res = itemView.getResources();
         }
 
         void bind(Material material) {
-
+            materialName.setText(material.getName());
+            materialNumber.setText(res.getString(R.string.material_number, material.getMaterialNr()));
         }
     }
 }
