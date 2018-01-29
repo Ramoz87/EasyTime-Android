ea87c4b4d829a65c7b9238f935d49e7846411650
Alexei <alexei.artsimovich@mobexs.com>
2018-01-29 15:32:25 +0300

    fixed in AddMaterials screen

--- app/src/main/java/com/example/paralect/easytime/main/projects/project/jobexpenses/materials/MaterialExpensesFragment.java
+++ app/src/main/java/com/example/paralect/easytime/main/projects/project/jobexpenses/materials/MaterialExpensesFragment.java
@@ -9,6 +9,7 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -20,8 +21,11 @@ import com.example.paralect.easytime.R;
 import com.example.paralect.easytime.main.BaseFragment;
 import com.example.paralect.easytime.model.Job;
 import com.example.paralect.easytime.model.Material;
+import com.example.paralect.easytime.utils.MetricsUtils;
 import com.example.paralect.easytime.views.EmptyRecyclerView;
 import com.example.paralect.easytime.views.KeypadEditorView;
+
+import net.cachapa.expandablelayout.ExpandableLayout;
 
 import java.util.List;
 
@@ -33,13 +37,14 @@ import butterknife.OnClick;
  * Created by alexei on 17.01.2018.
  */
 
-public class MaterialExpensesFragment extends BaseFragment implements IMaterialExpenses<List<Material>>, MaterialExpensesAdapter.OnCheckedCountChangeListener {
+public class MaterialExpensesFragment extends BaseFragment implements IMaterialExpenses<List<Material>>, MaterialExpensesAdapter.OnCheckedCountChangeListener, ExpandableLayout.OnExpansionUpdateListener {
     private static final String TAG = MaterialExpensesFragment.class.getSimpleName();
 
     public static final String ARG_KEYBOARD_STATE = "keyboard_state";
 
     @BindView(R.id.addMaterials) Button addMaterials;
     @BindView(R.id.list) EmptyRecyclerView emptyRecyclerView;
+    @BindView(R.id.editor_layout) View editorLayout;
     private KeypadEditorView keypadEditorView;
 
     private Job job;
@@ -164,4 +169,34 @@ public class MaterialExpensesFragment extends BaseFragment implements IMaterialE
         backForOneStep();
     }
     // endregion
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        keypadEditorView.setOnExpansionUpdateListener(null);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        keypadEditorView.setOnExpansionUpdateListener(this);
+    }
+
+    @Override
+    public void onExpansionUpdate(float expansionFraction, int state) {
+        Log.d(TAG, "expansion update: fraction = " + expansionFraction);
+        int paddingTop = emptyRecyclerView.getPaddingTop();
+        int paddingLeft = emptyRecyclerView.getPaddingLeft();
+        int paddingRight = emptyRecyclerView.getPaddingRight();
+        int buttonLayoutHeight = editorLayout.getMeasuredHeight();
+        int navBarHeight = (int) MetricsUtils.convertDpToPixel(64);
+        int paddingBottom = (int) (keypadEditorView.getMeasuredHeight() * expansionFraction);
+        Log.d(TAG, String.format("Button layout height = %s, nav bar height = %s, keypad editor view = %s", buttonLayoutHeight, navBarHeight, paddingBottom));
+        paddingBottom = paddingBottom - buttonLayoutHeight - navBarHeight;
+        if (paddingBottom <= 0) {
+            paddingBottom = 0;
+        }
+        Log.d(TAG, String.format("padding bottom = %s", paddingBottom));
+        emptyRecyclerView.setPadding(paddingLeft, paddingTop, paddingRight, paddingBottom);
+    }
 }
